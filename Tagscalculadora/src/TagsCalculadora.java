c6065576b9995a87111142c0068fcdb9ebabbd3d
Augusto Liberato <augusto.liberato10@gmail.com>
2024-09-05 12:11:44 -0300

    Comentar

---
+++ Tagscalculadora/src/TagsCalculadora.java
@@ -0,0 +1,22 @@
+/**
+* @author Augusto Liberato
+* @version 1.0
+* @since 01/01/2022
+*/
+
+ 
+ 
+public class TagsCalculadora {
+
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é  o segundo parâmetro do método
+     * @return int o resultado deste método é a soma do dois números
+     */
+    public int somar(int numeroUm, int numeroDois){//solocar o mouse em cima de somar
+        return numeroUm + numeroDois;
+    }
+
+    
+}
